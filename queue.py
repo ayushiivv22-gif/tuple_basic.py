941a0274977e9c788ae7818ba2ba6871cb113067
ayushiivv22-gif <vermaayushi0107@gmail.com>
2026-03-23 19:14:55 +0530

    Implement basic FIFO queue using list
    basic queue program using list.

---
+++ queue.py
@@ -0,0 +1,14 @@
+# Queue using list (FIFO)
+
+queue = []
+
+# enqueue
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+print("Queue:", queue)
+
+# dequeue
+queue.pop(0)
+print("After dequeue:", queue)
